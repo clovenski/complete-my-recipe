b5a4ae32661383fdf307bce9b2dbc20199bce450
Joel Tengco <joeltengco@yahoo.com>
2019-02-28 23:10:59 -0800

    tested requesting api from javascript

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,11 @@
 // implement buttons, drop down menus and such
+
+// example code for using API, assuming some data in database already
+// var request = new XMLHttpRequest();
+// request.open('GET', 'http://localhost:8000/recipes/', true);
+// request.onload = function() {
+//     var json = JSON.parse(request.responseText);
+//     console.log(json[0].name);
+// };
+
+// request.send(null);
