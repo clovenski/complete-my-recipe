6e600a6f7840e7c055eeaa388074da2d29fe5581
Joel Tengco <joeltengco@yahoo.com>
2019-03-04 18:00:35 -0800

    add category for each ignredient

--- django/api/completemyrecipe/models.py
+++ django/api/completemyrecipe/models.py
@@ -12,7 +12,15 @@ class Recipe(models.Model):
         return str(self.name)
 
 class Ingredient(models.Model):
+    CATEGORIES = (
+        ('P', 'Protein'),
+        ('G', 'Grain'),
+        ('F', 'Fruit'),
+        ('V', 'Vegetable'),
+    )
+
     name = models.CharField(max_length=50)
+    category = models.CharField(max_length=1, choices=CATEGORIES, default=('U', 'Unknown'))
 
     def __str__(self):
         return str(self.name)
