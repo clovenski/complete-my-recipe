1b20edaba210acc88bb0d233fc55e7dbf39f1f60
Joel Tengco <joeltengco@yahoo.com>
2019-04-28 19:39:19 -0700

    comment out search ingredient feature for dev after deployment

--- scripts/home_page.js
+++ scripts/home_page.js
@@ -1,50 +1,50 @@
 var queryParams = new Set([]);
-var delayTimer = null;
+// var delayTimer = null;
 
-function searchIngredient(name) {
-    if (name.length == 0) {
-        clearTimeout(delayTimer);
-        document.getElementById("suggestions").innerHTML = "Suggestions pop up here.";
-    } else {
-        clearTimeout(delayTimer);
-        delayTimer = setTimeout(function() {
-            var request = new XMLHttpRequest();
-            request.onreadystatechange = function() {
-                if (this.readyState == 4 && this.status == 200) {
-                    document.getElementById("suggestions").innerHTML = "";
-                    response = JSON.parse(this.responseText);
-                    if (response.length == 0) {
-                        document.getElementById("suggestions").innerHTML = "No matches.";
-                    }
-                    for (var i = 0; i < response.length; i++) {
-                        var ingredient = response[i]["name"];
-                        if (queryParams.has(ingredient)) {
-                            continue;
-                        }
-                        var suggestion = document.createElement("button");
-                        suggestion.id = ingredient;
-                        suggestion.setAttribute("class", "suggestion");
-                        suggestion.setAttribute("onclick", "addIngredient(this.id)");
-                        suggestion.innerHTML = response[i]["name"];
-                        document.getElementById("suggestions").appendChild(suggestion);
-                    }
-                }
-            };
-            request.open("GET", "/data/ingredients/?search=" + name, true);
-            request.send();
-        },1500);
-    }
-}
+// function searchIngredient(name) {
+//     if (name.length == 0) {
+//         clearTimeout(delayTimer);
+//         document.getElementById("suggestions").innerHTML = "Suggestions pop up here.";
+//     } else {
+//         clearTimeout(delayTimer);
+//         delayTimer = setTimeout(function() {
+//             var request = new XMLHttpRequest();
+//             request.onreadystatechange = function() {
+//                 if (this.readyState == 4 && this.status == 200) {
+//                     document.getElementById("suggestions").innerHTML = "";
+//                     response = JSON.parse(this.responseText);
+//                     if (response.length == 0) {
+//                         document.getElementById("suggestions").innerHTML = "No matches.";
+//                     }
+//                     for (var i = 0; i < response.length; i++) {
+//                         var ingredient = response[i]["name"];
+//                         if (queryParams.has(ingredient)) {
+//                             continue;
+//                         }
+//                         var suggestion = document.createElement("button");
+//                         suggestion.id = ingredient;
+//                         suggestion.setAttribute("class", "suggestion");
+//                         suggestion.setAttribute("onclick", "addIngredient(this.id)");
+//                         suggestion.innerHTML = response[i]["name"];
+//                         document.getElementById("suggestions").appendChild(suggestion);
+//                     }
+//                 }
+//             };
+//             request.open("GET", "/data/ingredients/?search=" + name, true);
+//             request.send();
+//         },1500);
+//     }
+// }
 
-function addIngredient(name) {
-    queryParams.add(name);
-    document.getElementById("searchButton").disabled = false;
-    var ingredient = document.getElementById(name);
-    document.getElementById("suggestions").removeChild(ingredient);
-    document.getElementById("userIngredients").appendChild(ingredient);
-    ingredient.setAttribute("onclick", "removeIngredient(this.id)");
-    ingredient.setAttribute("class", "userIngredient");
-}
+// function addIngredient(name) {
+//     queryParams.add(name);
+//     document.getElementById("searchButton").disabled = false;
+//     var ingredient = document.getElementById(name);
+//     document.getElementById("suggestions").removeChild(ingredient);
+//     document.getElementById("userIngredients").appendChild(ingredient);
+//     ingredient.setAttribute("onclick", "removeIngredient(this.id)");
+//     ingredient.setAttribute("class", "userIngredient");
+// }
 
 function removeIngredient(name) {
     queryParams.delete(name);
