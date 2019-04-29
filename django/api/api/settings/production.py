d65654ab435849ff792e37b9f8b82421c76a3b9a
Joel Tengco <joeltengco@yahoo.com>
2019-04-28 20:24:50 -0700

    edit production settings according to deploy checklist

--- django/api/api/settings/production.py
+++ django/api/api/settings/production.py
@@ -5,3 +5,9 @@ DEBUG = False
 ALLOWED_HOSTS = ['127.0.0.1' ,'localhost', 'completemyrecipe.pythonanywhere.com']
 
 CORS_ORIGIN_WHITELIST = ['127.0.0.1:8000' , 'localhost:8000', 'completemyrecipe.pythonanywhere.com']
+
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_BROWSER_XSS_FILTER = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+X_FRAME_OPTIONS = 'DENY'
